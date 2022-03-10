13d13b83a473a772b8df381cf5b74a3204dad9e1
Stewart Jester <jesterstewart@gmail.com>
2022-03-10 09:25:27 -0800

    added an s

--- app/public/wp-config.php
+++ app/public/wp-config.php
@@ -37,7 +37,7 @@ define( 'DB_CHARSET', 'utf8' );
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
-/** enable debug mode */
+/** enable debug mode s */
 define('WP_DEBUG', true);
 
 /** log debug info */
